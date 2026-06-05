ca8aad5cc2288c2359b9e906f16e33da7114b8d4
Bruno Turcksin <bruno.turcksin@gmail.com>
2026-06-05 14:23:28 -0400

    Add Caliper instrumentation

--- source/ThermalPhysics.templates.hh
+++ source/ThermalPhysics.templates.hh
@@ -861,8 +861,11 @@ double ThermalPhysics<dim, n_materials, p_order, fe_degree, MaterialStates,
         dealii::LA::distributed::Vector<double, MemorySpaceType> &solution,
         std::vector<Timer> &timers)
 {
+#ifdef ADAMANTINE_WITH_CALIPER
+  CALI_CXX_MARK_FUNCTION;
+#endif
   // For very small time steps (e.g., less than 1e-4 second), using deal.II to
-  // perform a forward steps becomes costly. In that case, we just peform the
+  // perform a forward steps becomes costly. In that case, we just perform the
   // forward euler ourselves.
   if (_forward_euler)
   {
