98f92425f5ac42bbc684699e7f4a209682df8dc4
Bruno Turcksin <bruno.turcksin@gmail.com>
2026-06-08 10:17:07 -0400

    Update figures

--- performance/dwell_rook/analysis.py
+++ performance/dwell_rook/analysis.py
@@ -4,77 +4,111 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 import numpy as np
 
-colors = ['#E69F00', '#56B4E9', '#009E73', '#CC79A7']
+colors = ['#E69F00', '#56B4E9', '#009E73', '#0072b2', '#CC79A7']
 color_coarse = colors[0]
-color_fine = colors[1]
-color_p2 = colors[2]
-color_ideal = colors[3]
+color_fine = colors[2]
+color_p2 = colors[1]
+color_p3 = colors[3]
+color_ideal = colors[4]
 
 n_procs = [1, 2, 4, 8, 12]
 
 # Coarse Mesh
 #------------
-time_coarse = [9266.1, 6617.7, 4647.7, 4571.9, 4825.8]
-ideal_strong_time_coarse = [9266.1, 9266.1/2, 9266.1/4, 9266.1/8, 9266.1/12]
+time_coarse = [6031.9, 4320.7, 3020.7, 3026.3, 3252.9]
+time_coarse_0 = time_coarse[0]
+ideal_strong_time_coarse = [time_coarse_0, time_coarse_0/2, time_coarse_0/4,
+        time_coarse_0/8, time_coarse_0/12]
 
-speedup_coarse = [9266.1/9266.1, 9266.1/6617.7, 9266.1/4647.7, 9266.1/4571.9, 9266.1/4825.8]
-efficiency_coarse = [9266.1/9266.1, 9266.1/(6617.7*2), 9266.1/(4647.7*4),
-        9266.1/(4571.9*8), 9266.1/(4825.8*12)]
+speedup_coarse = [time_coarse_0/x for x in time_coarse]
+efficiency_coarse = [time_coarse_0/time_coarse_0, time_coarse_0/(time_coarse[1]*2),
+        time_coarse_0/(time_coarse[2]*4), time_coarse_0/(time_coarse[3]*8),
+        time_coarse_0/(time_coarse[4]*12)]
 
-min_throughput_coarse = [903.5, 1580.4, 2545.2, 2549.4, 1877.2]
-max_throughput_coarse = [3958.1, 4648.5, 4705.7, 4329.8, 3935.5]
-average_throughput_coarse = [1842.9, 2580.4, 3674.2, 3735.2, 3538.7]
-realtime_throughput_coarse = [1538.4, 1538.4, 1538.4, 1538.4, 1538.4]
-first_block_throughput_coarse = [3132.9, 4426.9, 4510.9, 4154.5, 3657.4]
-last_block_throughput_coarse = [1322.0, 2000.3, 3106.2, 3422.5, 3364.4]
-average_throughput_speedup_coarse = [1842.9/1842.9, 2580.4/1842.9, 3674.2/1842.9, 3735.2/1842.9, 3538.7/1842.9]
-first_block_throughput_speedup_coarse = [3132.9/3132.9, 4426.9/3132.9, 4510.9/3132.9, 4154.5/3132.9,
-        3657.4/3132.9]
-last_block_throughput_speedup_coarse = [1322.0/1322.0, 2000.3/1322.0, 3106.2/1322.0, 3422.5/1322.0,
-        3364.4/1322.0]
+average_throughput_coarse = [1840.1, 2568.8, 3674.4, 3667.6, 3412.1]
+realtime_throughput_coarse = [1000, 1000, 1000, 1000, 1000]
+first_block_throughput_coarse = [3106.7, 4407.5, 4498.6, 4067.8, 3537.2]
+last_block_throughput_coarse = [1314.9, 1996.9, 3100.4, 3350.6, 3228.2]
+average_throughput_coarse_0 = average_throughput_coarse[0]
+average_throughput_speedup_coarse = [x/average_throughput_coarse_0 for x in average_throughput_coarse]
+first_throughput_coarse_0 = first_block_throughput_coarse[0]
+first_block_throughput_speedup_coarse = [x/first_throughput_coarse_0 for x in first_block_throughput_coarse]
+last_throughput_coarse_0 = last_block_throughput_coarse[0]
+last_block_throughput_speedup_coarse = [x/last_throughput_coarse_0 for x in last_block_throughput_coarse]
 
-eval_percent_coarse = [76.24, 67.0, 70.1, 61.3, 58.6]
+eval_percent_coarse = [75.8, 66.6, 69.6, 60.9, 58.4]
 
 # Fine Mesh
 #----------
-time_fine = [42928.1, 24310.6, 13864.4, 9339.8, 9322.2]
-ideal_strong_time_fine = [42928.1, 42928.1/2 ,42928.1/4, 42928.1/8, 42928.1/12]
+time_fine = [27993.6, 15656.3, 10166.0, 6267.9, 5691.6]
+time_fine_0 = time_fine[0]
+ideal_strong_time_fine = [time_fine_0, time_fine_0/2, time_fine_0/4,
+        time_fine_0/8, time_fine_0/12]
 
-speedup_fine = [42928.1/42928.1, 42928.1/24310.6, 42928.1/13864.4, 42928.1/9339.8, 42928.1/9322.2]
-efficiency_fine = [42928.1/42928.1, 42928.1/(24310.6*2), 42928.1/(13864.4*4),
-        42928.1/(9339.8*8), 42928.1/(9322.2*12)] 
+speedup_fine = [time_fine_0/x for x in time_fine]
+efficiency_fine = [time_fine_0/time_fine_0, time_fine_0/(time_fine[1]*2),
+        time_fine_0/(time_fine[2]*4), time_fine_0/(time_fine[3]*8),
+        time_fine_0/(time_fine[4]*12)]
 
-min_throughput_fine = [1.5, 1.8, 13.5, 1.9, 1.8]
-max_throughput_fine = [1264.7, 1783.9, 2669.0, 2904.3, 2923.3]
-average_throughput_fine = [397.8, 702.4, 1231.7, 1828.4, 1831.8]
-first_block_throughput_fine = [1124.9, 1611.5, 2434.4, 2627.7, 2581.9]
-last_block_throughput_fine = [240.7, 449.2, 810.7, 1386.1, 1215.0]
-average_throughput_speedup_fine = [397.8/397.8, 702.4/397.8, 1231.7/397.8, 1828.4/397.8, 1831.8/397.8]
-first_block_throughput_speedup_fine = [1124.9/1124.9, 1611.5/1124.9, 2434.4/1124.9, 2627.7/1124.9, 2581.9/1124.9]
-last_block_throughput_speedup_fine = [240.7/240.7, 449.2/240.7, 810.7/240.7, 1386.1/240.7, 1215.0/240.7]
+average_throughput_fine = [396.5, 708.9, 1091.8, 1770.8, 1950.1]
+first_block_throughput_fine = [1188.5, 1752.5, 2247.6, 2525.9, 2497.7]
+last_block_throughput_fine = [239.0, 444.9, 733.0, 1327.0, 1530.8]
+average_throughput_fine_0 = average_throughput_fine[0]
+average_throughput_speedup_fine = [x/average_throughput_fine_0 for x in average_throughput_fine]
+first_throughput_fine_0 = first_block_throughput_fine[0]
+first_block_throughput_speedup_fine = [x/first_throughput_fine_0 for x in first_block_throughput_fine]
+last_throughput_fine_0 = last_block_throughput_fine[0]
+last_block_throughput_speedup_fine = [x/last_throughput_fine_0 for x in last_block_throughput_fine]
 
-eval_percent_fine = [93.6, 88.7, 87.5, 77.8, 73.5]
+eval_percent_fine = [95.3, 90.7, 86.7, 77.5, 72.1]
 
-# P-Refinement
-#-------------
-time_p2 = [19202.9, 12057.6, 7153.9, 6141.0, 5926.4]
-ideal_strong_time_p2 = [19202.9, 19202.9/2, 19202.9/4, 19202.9/8, 19202.9/12]
+# P2-Refinement
+#--------------
+time_p2 = [12682.6, 7836.5, 4757.7, 4037.8, 3927.9]
+time_p2_0 = time_p2[0]
+ideal_strong_time_p2 = [time_p2_0, time_p2_0/2, time_p2_0/4,
+        time_p2_0/8, time_p2_0/12]
 
-speedup_p2 = [19202.9/19202.9, 19202.9/12057.6, 19202.9/7153.9, 19202.9/6141.0,
-        19202.9/5926.4]
-efficiency_p2 = [19202.9/19202.9, 19202.9/(12057.6*2), 19202.9/(7153.9*4),
-        19202.9/(6141.0*8), 19202.9/(5926.4*12)] 
+speedup_p2 = [time_p2_0/x for x in time_p2]
+efficiency_p2 = [time_p2_0/time_p2_0, time_p2_0/(time_p2[1]*2),
+        time_p2_0/(time_p2[2]*4), time_p2_0/(time_p2[3]*8),
+        time_p2_0/(time_p2[4]*12)]
 
-min_throughput_p2 = [355.6, 694.8, 434.0, 1756.5, 1560.6]
-max_throughput_p2 = [2303.9, 2748.1, 4230.7, 3809.2, 3770.7]
-average_throughput_p2 = [889.2, 1416.2, 2387.1, 2780.8, 2881.5]
-first_block_throughput_p2 = [2118.2, 2592.5, 3948.4, 3650.0, 3292.0]
-last_block_throughput_p2 = [572.0, 983.5, 1733.9, 2253.9, 2475.3]
-average_throughput_speedup_p2 = [889.2/889.2, 1416.2/889.2, 2387.1/889.2, 2780.8/889.2, 2881.5/889.2]
-first_block_throughput_speedup_p2 = [2118.2/2118.2, 2592.5/2118.2, 3948.4/2118.2, 3650.0/2118.2, 3292.0/2118.2]
-last_block_throughput_speedup_p2 = [572.0/572.0, 983.5/572.0, 1733.9/572.0, 2253.9/572.0, 2475.3/572.0]
+average_throughput_p2 = [875.1, 1415.3, 2332.9, 2748.8, 2825.7]
+first_block_throughput_p2 = [2097.4, 2682.0, 3870.1, 3602.9, 3326.9]
+last_block_throughput_p2 = [564.3, 978.2, 1690.4, 2230.8, 2427.2]
+average_throughput_p2_0 = average_throughput_p2[0]
+average_throughput_speedup_p2 = [x/average_throughput_p2_0 for x in average_throughput_p2]
+first_throughput_p2_0 = first_block_throughput_p2[0]
+first_block_throughput_speedup_p2 = [x/first_throughput_p2_0 for x in first_block_throughput_p2]
+last_throughput_p2_0 = last_block_throughput_p2[0]
+last_block_throughput_speedup_p2 = [x/last_throughput_p2_0 for x in last_block_throughput_p2]
 
-eval_percent_fine = [87.9, 79.6, 79.6, 68.9, 64.6]
+eval_percent_fine = [87.8, 79.7, 79.2, 68.7, 64.2]
+
+# P3-Refinement
+#--------------
+time_p3 = [29076.8, 19321.7, 12362.0, 6293.8, 5507.1]
+time_p3_0 = time_p3[0]
+ideal_strong_time_p3 = [time_p3_0, time_p3_0/2, time_p3_0/4,
+        time_p3_0/8, time_p3_0/12]
+
+speedup_p3 = [time_p3_0/x for x in time_p3]
+efficiency_p3 = [time_p3_0/time_p3_0, time_p3_0/(time_p3[1]*2),
+        time_p3_0/(time_p3[2]*4), time_p3_0/(time_p3[3]*8),
+        time_p3_0/(time_p3[4]*12)]
+
+average_throughput_p3 = [381.7, 574.4, 897.8, 1763.5, 2015.4]
+first_block_throughput_p3 = [1257.6, 2065.9, 2573.2, 2970.6, 2886.9]
+last_block_throughput_p3 = [203.3, 439.4, 519.7, 1251.8, 1528.3]
+average_throughput_p3_0 = average_throughput_p3[0]
+average_throughput_speedup_p3 = [x/average_throughput_p3_0 for x in average_throughput_p3]
+first_throughput_p3_0 = first_block_throughput_p3[0]
+first_block_throughput_speedup_p3 = [x/first_throughput_p3_0 for x in first_block_throughput_p3]
+last_throughput_p3_0 = last_block_throughput_p3[0]
+last_block_throughput_speedup_p3 = [x/last_throughput_p3_0 for x in last_block_throughput_p3]
+
+eval_percent_fine = [94.0, 91.9, 88.0, 76.5, 71.6]
 
 ######################
 # PLOT PARAMETERS
@@ -93,16 +127,21 @@ ideal_strong_coarse_plt, = plt.loglog(n_procs, ideal_strong_time_coarse, '--',
         color = color_ideal, linewidth = 3, markersize = 12,
         label='Ideal Strong Scaling')
 time_fine_plt, = plt.loglog(n_procs, time_fine, '*-', color = color_fine, linewidth = 3, markersize = 12,
-        label='H-refined Simulation')
+        label='h-refined Simulation')
 ideal_strong_fine_plt, = plt.loglog(n_procs, ideal_strong_time_fine, '--', color
         = color_ideal, linewidth = 3, markersize = 12,
         label='Ideal Strong Scaling')
 time_p2_plt, = plt.loglog(n_procs, time_p2, '*-',  color = color_p2, linewidth = 3, markersize = 12,
-        label='P-refine Simulation')
+        label='p2-refine Simulation')
 ideal_strong_p2_plt, = plt.loglog(n_procs, ideal_strong_time_p2, '--', color =
         color_ideal, linewidth = 3, markersize = 12,
         label='Ideal Strong Scaling')
-ax.legend(handles=[time_coarse_plt, time_fine_plt, time_p2_plt, ideal_strong_coarse_plt], fontsize = 18)
+time_p3_plt, = plt.loglog(n_procs, time_p3, '*-',  color = color_p3, linewidth = 3, markersize = 12,
+        label='p3-refine Simulation')
+ideal_strong_p3_plt, = plt.loglog(n_procs, ideal_strong_time_p3, '--', color =
+        color_ideal, linewidth = 3, markersize = 12,
+        label='Ideal Strong Scaling')
+ax.legend(handles=[time_coarse_plt, time_fine_plt, time_p2_plt, time_p3_plt, ideal_strong_coarse_plt], fontsize = 18)
 plt.xlabel('Number of cores', fontsize = 20)
 plt.ylabel('Time (s)', fontsize = 20)
 ax.grid(which='both')
@@ -119,13 +158,20 @@ speedup_coarse_plt, = plt.plot(n_procs, speedup_coarse, '-', color =
         label='Coarse Simulation')
 speedup_fine_plt, = plt.plot(n_procs, speedup_fine, '-', color =
         color_fine, linewidth = 3, markersize = 12,
-        label='H-refined Simulation')
+        label='h-refined Simulation')
 speedup_p2_plt, = plt.plot(n_procs, speedup_p2, '-', color =
         color_p2, linewidth = 3, markersize = 12,
-        label='P-refined Simulation')
+        label='p2-refined Simulation')
+speedup_p3_plt, = plt.plot(n_procs, speedup_p3, '-', color =
+        color_p3, linewidth = 3, markersize = 12,
+        label='p3-refined Simulation')
 ideal_speedup_plt, = plt.plot(n_procs, n_procs , '-', color = color_ideal, linewidth = 3, markersize = 12,
         label='Ideal Scaling')
-ax.legend(handles=[speedup_coarse_plt, speedup_fine_plt, speedup_p2_plt,  ideal_speedup_plt], fontsize = 18)
+plt.xlim(1, 12)
+plt.ylim(1, 8)
+plt.xticks(np.arange(1, 13, 1))
+plt.yticks(np.arange(1, 9, 1))
+ax.legend(handles=[speedup_coarse_plt, speedup_fine_plt, speedup_p2_plt, speedup_p3_plt, ideal_speedup_plt], fontsize = 18)
 plt.xlabel('Number of cores', fontsize = 20)
 plt.ylabel('Speedup', fontsize = 20)
 ax.grid(which='both')
@@ -140,44 +186,54 @@ fig, ax = plt.subplots(figsize=(16, 12), dpi = 100)
 
 avg_coarse_plt, = plt.plot(n_procs, average_throughput_coarse, '-', color =
         color_coarse, linewidth = 3, markersize = 12,
-        label='Average: Coarse')
+        label='Coarse: Average')
 first_coarse_plt, = plt.plot(n_procs, first_block_throughput_coarse, '-.', color
         = color_coarse, linewidth = 3, 
-        markersize = 12, label='First Block: Coarse')
+        markersize = 12, label='Coarse: First Block')
 last_coarse_plt, = plt.plot(n_procs, last_block_throughput_coarse, '--', color =
         color_coarse, linewidth = 3, 
-        markersize = 12, label='Last Block: Coarse')
+        markersize = 12, label='Coarse: Last Block')
 
 avg_fine_plt, = plt.plot(n_procs, average_throughput_fine, '-', color =
         color_fine, linewidth = 3, markersize = 12,
-        label='Average: H-refine')
+        label='h-refined: Average')
 first_fine_plt, = plt.plot(n_procs, first_block_throughput_fine, '-.', color =
         color_fine, linewidth = 3, 
-        markersize = 12, label='First Block: H-refine')
+        markersize = 12, label='h-refined: First Block')
 last_fine_plt, = plt.plot(n_procs, last_block_throughput_fine, '--', color =
         color_fine, linewidth = 3, 
-        markersize = 12, label='Last Block: H-refine')
+        markersize = 12, label='h-refined: Last Block')
 
 avg_p2_plt, = plt.plot(n_procs, average_throughput_p2, '-', color =
         color_p2, linewidth = 3, markersize = 12,
-        label='Average: P-refine')
+        label='p2-refined: Average')
 first_p2_plt, = plt.plot(n_procs, first_block_throughput_p2, '-.',
         color = color_p2, linewidth = 3, 
-        markersize = 12, label='First Block: P-refine')
+        markersize = 12, label='p2-refined: First Block')
 last_p2_plt, = plt.plot(n_procs, last_block_throughput_p2, '--', color
         = color_p2, linewidth = 3, 
-        markersize = 12, label='Last Block: P-refine')
+        markersize = 12, label='p2-refined: Last Block')
+
+avg_p3_plt, = plt.plot(n_procs, average_throughput_p3, '-', color =
+        color_p3, linewidth = 3, markersize = 12,
+        label='p-refined: Average')
+first_p3_plt, = plt.plot(n_procs, first_block_throughput_p3, '-.',
+        color = color_p3, linewidth = 3, 
+        markersize = 12, label='p3-refined: First Block')
+last_p3_plt, = plt.plot(n_procs, last_block_throughput_p3, '--', color
+        = color_p3, linewidth = 3, 
+        markersize = 12, label='p3-refined: Last Block')
 
 realtime_coarse_plt, = plt.plot(n_procs, realtime_throughput_coarse, ':', color
         = color_ideal, linewidth = 3, markersize = 12, label='Real Time')
 ax.legend(handles=[first_coarse_plt, avg_coarse_plt, last_coarse_plt,
     first_fine_plt, avg_fine_plt, last_fine_plt, first_p2_plt, avg_p2_plt,
-    last_p2_plt, realtime_coarse_plt], ncols = 2, fontsize = 18)
+    last_p2_plt, first_p3_plt, avg_p3_plt, last_p3_plt, realtime_coarse_plt], ncols = 3, fontsize = 18)
 #plt.title('Throughput' , fontsize = 20)
 plt.xlabel('Number of cores', fontsize = 20)
 plt.ylabel('Iterations/s', fontsize = 20)
 plt.xlim(1, 12)
-plt.ylim(0, 4800)
+plt.ylim(0, 5900)
 ax.xaxis.set_major_locator(MultipleLocator(1))
 ax.yaxis.set_major_locator(MultipleLocator(500))
 ax.grid(True)
@@ -192,39 +248,53 @@ fig, ax = plt.subplots(figsize=(16, 12), dpi = 100)
 
 avg_speedup_coarse_plt, = plt.plot(n_procs, average_throughput_speedup_coarse, '-', color =
         color_coarse, linewidth = 3, markersize = 12,
-        label='Average: Coarse')
+        label='Coarse: Average')
 first_speedup_coarse_plt, = plt.plot(n_procs, first_block_throughput_speedup_coarse, '-.', color
         = color_coarse, linewidth = 3, 
-        markersize = 12, label='First Block: Coarse')
+        markersize = 12, label='Coarse: First Block')
 last_speedup_coarse_plt, = plt.plot(n_procs, last_block_throughput_speedup_coarse, '--', color =
         color_coarse, linewidth = 3, 
-        markersize = 12, label='Last Block: Coarse')
+        markersize = 12, label='Coarse: Last Block')
 
 avg_speedup_fine_plt, = plt.plot(n_procs, average_throughput_speedup_fine, '-', color =
         color_fine, linewidth = 3, markersize = 12,
-        label='Average: H-refine')
+        label='h-refined: Average')
 first_speedup_fine_plt, = plt.plot(n_procs, first_block_throughput_speedup_fine, '-.', color =
         color_fine, linewidth = 3, 
-        markersize = 12, label='First Block: H-refine')
+        markersize = 12, label='h-refined: First Block')
 last_speedup_fine_plt, = plt.plot(n_procs, last_block_throughput_speedup_fine, '--', color =
         color_fine, linewidth = 3, 
-        markersize = 12, label='Last Block: H-refine')
+        markersize = 12, label='h-refined: Last Block')
 
 avg_speedup_p2_plt, = plt.plot(n_procs, average_throughput_speedup_p2, '-', color =
         color_p2, linewidth = 3, markersize = 12,
-        label='Average: P-refine')
+        label='Average: P2-refine')
 first_speedup_p2_plt, = plt.plot(n_procs, first_block_throughput_speedup_p2, '-.',
         color = color_p2, linewidth = 3, 
-        markersize = 12, label='First Block: P-refine')
+        markersize = 12, label='p2-refined: First Block')
 last_speedup_p2_plt, = plt.plot(n_procs, last_block_throughput_speedup_p2, '--', color
         = color_p2, linewidth = 3, 
-        markersize = 12, label='Last Block: P-refine')
+        markersize = 12, label='p2-refined: Last Block')
+
+avg_speedup_p3_plt, = plt.plot(n_procs, average_throughput_speedup_p3, '-', color =
+        color_p3, linewidth = 3, markersize = 12,
+        label='Average: P3-refine')
+first_speedup_p3_plt, = plt.plot(n_procs, first_block_throughput_speedup_p3, '-.',
+        color = color_p3, linewidth = 3, 
+        markersize = 12, label='p3-refined: First Block')
+last_speedup_p3_plt, = plt.plot(n_procs, last_block_throughput_speedup_p3, '--', color
+        = color_p3, linewidth = 3, 
+        markersize = 12, label='p3-refined: Last Block')
 ideal_speedup_plt, = plt.plot(n_procs, n_procs , '-', color = color_ideal, linewidth = 3, markersize = 12,
         label='Ideal Scaling')
 
+plt.xlim(1, 12)
+plt.ylim(1, 8.8)
+plt.xticks(np.arange(1, 13, 1))
+plt.yticks(np.arange(1, 9, 1))
 ax.legend(handles=[first_speedup_coarse_plt, avg_speedup_coarse_plt, last_speedup_coarse_plt,
     first_speedup_fine_plt, avg_speedup_fine_plt, last_speedup_fine_plt, first_speedup_p2_plt, avg_speedup_p2_plt,
-    last_speedup_p2_plt, ideal_speedup_plt],  ncols = 2, fontsize = 18)
+    last_speedup_p2_plt, first_speedup_p3_plt, avg_speedup_p3_plt, last_speedup_p3_plt, ideal_speedup_plt],  ncols = 2, fontsize = 18)
 #plt.title('Throughput' , fontsize = 20)
 plt.xlabel('Number of cores', fontsize = 20)
 plt.ylabel('Speedup', fontsize = 20)
